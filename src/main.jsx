286ba43de70e64dcd7264aa81c40163afd8ff04e
gaurav-s7ngh <gauravsinghguru07@gmail.com>
2026-03-01 02:49:38 +0530

    fixed guild connection

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/base.css' // <-- CHANGE THIS LINE
+import './index.css' // <-- ADD THIS LINE TO LOAD TAILWIND!
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
